246ab0dcb916352ac9ae381510febd57e57a42fe
Alamu Ramasamy <54072236+alamuramasamy9@users.noreply.github.com>
2022-11-09 20:02:31 -0500

    Delete interfaces directory

--- interfaces/BookmarkControllerI.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response} from "express";
-/**
- * @file Implements BookmarkController interface
- */
-export default interface BookmarkController {
-    findAllTuitsBookmarkedByUser (req: Request, res: Response): void;
-    userUnbookmarksTuit (req: Request, res: Response): void;
-    userBookmarksTuit (req: Request, res: Response): void;
-};
