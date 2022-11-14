2f269e33ca2ceb8f6900ec7029d098ff69cf558a
Alamu Ramasamy <54072236+alamuramasamy9@users.noreply.github.com>
2022-11-14 13:32:55 -0500

    Delete build directory

--- build/models/Follow.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * @file Declares Follow data type representing relationship between two
- * users, as in user follows another user
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-;
