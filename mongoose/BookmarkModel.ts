9586cfe894dee5a9b94abc1a42c8bd8e63a970ca
Alamu Ramasamy <54072236+alamuramasamy9@users.noreply.github.com>
2022-12-06 12:12:19 -0500

    Delete mongoose directory

--- mongoose/BookmarkModel.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file CRUD operations using
- * mongoose model in the bookmark collection
- */
-
- import mongoose from "mongoose";
- import BookmarkSchema from "./BookmarkSchema";
- const BookmarkModel = mongoose.model("BookmarkModel", BookmarkSchema);
- export default BookmarkModel;
--- mongoose/MessageModel.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file CRUD operations using
- * mongoose model in the message collection
- */
-
- import mongoose from "mongoose";
- import MessageSchema from "./MessageSchema";
- const MessageModel = mongoose.model("MessageModel", MessageSchema);
- export default MessageModel;
