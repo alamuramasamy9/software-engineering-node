7924d13d8ed1e4c51df5da770186ef8f042f3ab1
Alamu Ramasamy <54072236+alamuramasamy9@users.noreply.github.com>
2022-12-06 13:32:21 -0500

    Delete mongoose directory

--- mongoose/BookMarkModel.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @file Implements mongoose model to CRUD
- * documents in the BookMark collection
- */
- import mongoose from "mongoose";
- import BookMarkSchema from "./BookMarkSchema";
- const BookMarkModel = mongoose.model("BookMarkModel", BookMarkSchema);
- export default BookMarkModel;
